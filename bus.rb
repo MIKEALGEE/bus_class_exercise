58c1643790ed5841e304b4ff34542b0da8cb066f
ionawright <ionawright@live.com>
2018-09-18 15:15:31 +0100

    first commit

---
+++ bus.rb
@@ -0,0 +1,12 @@
+class Bus
+
+def initialize(number, destination)
+  @number = number
+  @destination = destination
+end
+
+def drive(drive)
+  return drive
+end
+
+end
---
+++ specs/bus_specs.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../bus.rb")
+
+
+class BusTest < MiniTest::Test
+
+  def setup()
+    @bus = Bus.new("22","Ocean Terminal")
+  end
+
+  def test_drive()
+    assert_equal("Brum Brum", @bus.drive("Brum Brum"))
+  end
+
+end
